0eda78fc0de16b626b726c5350cfe78efa3ed6a2
RWE1116 <41023228@gm.nfu.edu.tw>
2025-05-04 17:04:08 +0800

    downlond

--- pages/資料下載.py
+++ pages/資料下載.py
@@ -17,6 +17,6 @@ def download_file(file_path, file_label, mime_type, file_name):
 st.subheader("相關資料下載")
 
 # 下載文件2 (114年度下載資料)
-download_file(r"downloads/1301A_領隊_華外語_實務.pdf", "下載 1301A_領隊_華外語_實務.pdf", "application/pdf", "1301A_領隊_華外語_實務.pdf")
+download_file(r"1301A_領隊_華外語_實務.pdf", "下載 1301A_領隊_華外語_實務.pdf", "application/pdf", "1301A_領隊_華外語_實務.pdf")
 
-download_file(r"downloads/1301Q_領隊_華外語_實務.pdf", "下載 1301Q_領隊_華外語_實務.pdf", "application/pdf", "1301Q_領隊_華外語_實務.pdf")
+download_file(r"1301Q_領隊_華外語_實務.pdf", "下載 1301Q_領隊_華外語_實務.pdf", "application/pdf", "1301Q_領隊_華外語_實務.pdf")
